207e453154f7e0276a80e58151b2a6f97ec510bc
seiyamiyaoka <seiyamiyaoka@gmail.com>
2019-11-09 14:52:32 +0900

    feature: 暫定案

---
+++ main.js
@@ -0,0 +1,83 @@
+// mapしてkeyを追加
+// mapして階層が深い構造作成
+// filterで比較演算
+// filterを分ける
+// reduceの足し算
+// 初期値があるreduce
+// reduceを使ってArray<obj>の数値計算
+// 年齢ごとにgroup
+const users = [
+  {
+    id: 1,
+    name: 'tarou1',
+    age: 12
+  },
+  {
+    id: 2,
+    name: 'tarou2',
+    age: 13
+  },
+  {
+    id: 3,
+    name: 'tarou3',
+    age: 14
+  },
+  {
+    id: 5,
+    name: 'tarou1',
+    age: 12
+  },
+  {
+    id: 4,
+    name: 'tarou4',
+    age: 15
+  },
+]
+
+const newUser = users.map((user) => ({
+  ...user,
+  nameAge: `${user.name}: ${user.age}`
+}))
+
+// console.log(newUser)
+
+const over13ageUsers = users.filter((user) => user.age > 13)
+
+const isBigEnough = (user) => user.age > 13
+
+const separeteFuncUser = users.filter(isBigEnough)
+console.log(separeteFuncUser)
+
+const fruits = ['apple', 'banana', 'grapes', 'mango', 'orange'];
+
+const filterItems = (ary, query) => {
+  return ary.filter((el) => el.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+}
+
+console.log(filterItems(fruits, 'ap'))
+
+const ary1 = [1,2,3,4,5]
+
+const reducer = (prev, current) => prev + current
+
+console.log(ary1.reduce(reducer))
+console.log(ary1.reduce(reducer, 5))
+
+
+console.log(users.reduce((memo, currentObj) => {
+  return memo + currentObj.age
+}, 0))
+
+// うまくいかないからまた今度
+// const ageGroupCount = users.reduce((memo, currentObj) => {
+//   console.log(Object.keys(memo))
+//   console.log(String(currentObj.age))
+//   if(String(currentObj.age) in Object.keys(memo)) {
+//     memo[currentObj.age]++
+//   } else {
+//     memo[currentObj.age] = 1
+//   }
+//   return memo
+// }, {})
+
+// console.log(ageGroupCount)
---
+++ main.rb
@@ -0,0 +1,49 @@
+# クイックソートを実装
+# 10この乱数の配列を作成
+# require 'pry'
+# def randon_int
+#   (Random.rand * 100).to_i
+# end
+
+# def divid_three_conponent(ary_nums)
+#   return ary_nums if ary_nums.length <= 1
+#   left_content  = []
+#   right_content = []
+
+#   pivot_index = ary_nums.length / 2
+#   ary_nums.each_with_index do |_, n|
+#     next if n == pivot_index
+#     ary_nums[n] < ary_nums[pivot_index] ? left_content << ary_nums[n] : right_content << ary_nums[n]
+#   end
+#   return divid_three_conponent(left_content) + [ary_nums[pivot_index]] + divid_three_conponent(right_content)
+# end
+
+# ary = []
+# randon_int.times { ary << randon_int }
+
+# puts "初期配列: #{ary}"
+
+# p "並び替え配列: #{divid_three_conponent(ary)}"
+
+def random_init
+  (Random.rand * 100).to_i
+end
+
+def quick_sort(ary)
+  return ary if ary.size <= 1
+  left = []
+  right = []
+  pivot = ary.length / 2
+  ary.each_with_index do |_, i|
+    next if i == pivot
+    ary[i] < ary[pivot] ? left << ary[i] : right << ary[i]
+  end
+  return quick_sort(left) + [ary[pivot]] + quick_sort(right)
+end
+
+ary = []
+
+random_init.times { ary << random_init }
+
+p quick_sort(ary)
+
